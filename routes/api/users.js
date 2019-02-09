1762a14d9ee6738ebc6b359c0ded08b1adafd0e8
chloewhen117 <chloewhen21@gmail.com>
2019-02-08 23:41:47 -0800

    Completed register and login validators

--- routes/api/users.js
+++ routes/api/users.js
@@ -31,7 +31,8 @@ router.post('/register', (req, res) => {
   User.findOne({ email: req.body.email})
   .then(user => {
     if (user){
-      return res.status(400).json({email: 'Email already exists'});
+      errors.email = 'Email already exists';
+      return res.status(400).json(errors);
     } else {
       const avatar = gravatar.url(req.body.email, {
         s: '200', // Size
@@ -65,6 +66,13 @@ router.post('/register', (req, res) => {
 // @desc    Login User / Returning JWT route
 // @access  Public
 router.post('/login', (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  // Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   const email = req.body.email;
   const password = req.body.password;
 
@@ -72,8 +80,9 @@ router.post('/login', (req, res) => {
   User.findOne({email})
     .then(user => {
       // Check for user
-      if(!user){
-        return res.status(404).json({email: 'User email: not found'});
+      if (!user) {
+        errors.email = 'User not found';
+        return res.status(404).json(errors);
       }
 
       // Check Password
@@ -96,8 +105,8 @@ router.post('/login', (req, res) => {
                 })
               });
           } else {
-            return res.status(400).json({password: 'Password incorrect'});
-          }
+            errors.password = 'Password incorrect';
+            return res.status(400).json(errors);          }
         });
     });
 });
