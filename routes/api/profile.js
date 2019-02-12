1801e13df317c3a896785fd570a51915124861e0
chloewhen117 <chloewhen21@gmail.com>
2019-02-11 23:50:26 -0800

    Fixed routes, typos for profile to work correctly

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const passport = require('passport');
 
+// Load Validation
+const validateProfileInput = require('../../validation/profile');
+
 // Load Profile Model
 const Profile = require('../../models/Profile');
 // Load User Profile
@@ -12,6 +15,28 @@ const User = require('../../models/User');
 // @desc    Tests profile route
 // @access  Public
 router.get('/test', (req, res) => res.json({msg: "Profile Works"})
+);
+
+// @route   GET api/profile
+// @desc    Get current users profile
+// @access  Private
+router.get(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const errors = {};
+
+    Profile.findOne({ user: req.user.id })
+      .populate('user', ['name', 'avatar'])
+      .then(profile => {
+        if (!profile) {
+          errors.no_profile = 'There is no profile for this user';
+          return res.status(404).json(errors);
+        }
+        res.json(profile);
+      })
+      .catch(err => res.status(404).json(err));
+  }
 );
 
 // @route   POST api/profile
