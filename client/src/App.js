d6063f6367ec378f41a3e1c05da776938dbec042
chloewhen117 <chloewhen21@gmail.com>
2019-05-24 11:18:31 -0700

    return to previous state

--- client/src/App.js
+++ client/src/App.js
@@ -53,43 +53,45 @@ if(localStorage.jwtToken) {
 class App extends Component {
   render() {
     return (
-      <Provider store={ store }>
-        <Router>
-          <div className="App">
-            <Navbar />
-            <Route exact path="/" component={ Landing } />
-            <div>
-              <Route exact path="/register" component={ Register }/>
-              <Route exact path="/login" component={ Login }/>
-              <Route exact path="/profiles" component={ Profiles }/>
-              <Route exact path="/profile/:handle" component={Profile} />
-              <Switch>
-                <PrivateRoute exact path="/dashboard" component={ Dashboard }/>
-              </Switch>
-              <Switch>
-                <PrivateRoute exact path="/create-profile" component={ CreateProfile }/>
-              </Switch>
-              <Switch>
-                <PrivateRoute exact path="/edit-profile" component={ EditProfile }/>
-              </Switch>
-              <Switch>
-                <PrivateRoute exact path="/add-experience" component={ AddExperience }/>
-              </Switch>
-              <Switch>
-                <PrivateRoute exact path="/add-education" component={ AddEducation }/>
-              </Switch>
-              <Switch>
-                <PrivateRoute exact path="/post/:id" component={ Post } />
-              </Switch>
-	      <Switch>	
-                <PrivateRoute exact path="/feed" component={ Posts } />
-              </Switch>
-              <Route exact path="/not-found" component={ NotFound } />
-            </div>
-            <Footer />
-          </div>
-        </Router> 
-      </Provider>
+      <div className="wrapper">
+          <Provider store={ store }>
+            <Router>
+              <div className="App">
+                <Navbar />
+                <Route exact path="/" component={ Landing } />
+                <div className="content">
+                  <Route exact path="/register" component={ Register }/>
+                  <Route exact path="/login" component={ Login }/>
+                  <Route exact path="/profiles" component={ Profiles }/>
+                  <Route exact path="/profile/:handle" component={Profile} />
+                  <Switch>
+                    <PrivateRoute exact path="/dashboard" component={ Dashboard }/>
+                  </Switch>
+                  <Switch>
+                    <PrivateRoute exact path="/create-profile" component={ CreateProfile }/>
+                  </Switch>
+                  <Switch>
+                    <PrivateRoute exact path="/edit-profile" component={ EditProfile }/>
+                  </Switch>
+                  <Switch>
+                    <PrivateRoute exact path="/add-experience" component={ AddExperience }/>
+                  </Switch>
+                  <Switch>
+                    <PrivateRoute exact path="/add-education" component={ AddEducation }/>
+                  </Switch>
+                  <Switch>
+                    <PrivateRoute exact path="/post/:id" component={ Post } />
+                  </Switch>
+                  <Switch>	
+                    <PrivateRoute exact path="/feed" component={ Posts } />
+                  </Switch>
+                  <Route exact path="/not-found" component={ NotFound } />
+                </div>
+                <Footer />
+              </div>
+            </Router> 
+        </Provider>
+      </div>
     );
   }
 }
